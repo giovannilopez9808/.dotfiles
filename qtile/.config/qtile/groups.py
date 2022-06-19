063e4a738f9de3033bf879913b7107f87366d467
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-06-19 16:54:19 -0500

    Add windows move through workspace

--- qtile/.config/qtile/groups.py
+++ qtile/.config/qtile/groups.py
@@ -1,21 +1,88 @@
 from libqtile.config import Group, Key
-from keys import keys,super_key
+from keys import keys, super_key
 from libqtile.lazy import lazy
+from libqtile import qtile
 
+
+def define_groups() -> dict:
+    Groups_name = [number
+                   for number in "123456789"]
+    groups = dict()
+    for j, number in enumerate(Groups_name):
+        i = j-1
+        k = j+1
+        if i < 0:
+            i = len(Groups_name)-1
+        if k == len(Groups_name):
+            k = 0
+        groups[number] = {
+            "next": k,
+            "prev": i,
+        }
+    return groups
+
+
+# def move_window_to_screen(qtile, window, screen):
+    # """Moves a window to a screen and focuses it, allowing you to move it
+    # further if you wish."""
+    # window.togroup(screen.group.name)
+    # qtile.focus_screen(screen.index)
+    # screen.group.focus(window, True)
+
+
+# @lazy.function
+# def move_window_to_next_screen(qtile):
+    # """Moves a window to the next screen. Loops around the beginning and
+    # end."""
+    # index = qtile.current_group.name
+    # logger.warning(f"Current group {index}")
+    # index = qtile.current_screen.index
+    # logger.warning(f"Current window {index}")
+    # index = index + 1 if index < len(qtile.screens) - 1 else 0
+    # logger.warning(f"To window {index}")
+    # move_window_to_screen(qtile, qtile.current_window, qtile.screens[index])
+
+
+@lazy.function
+def displace_windows(qtile, groups_info: dict, move: str):
+    # Current workspace
+    index = qtile.current_group.name
+    # Update workspace
+    move_group = groups_info[index][move]
+    move_group = qtile.groups[move_group]
+    # Move windows
+    qtile.current_window.togroup(move_group.name)
+    # Move workspace
+    qtile.current_screen.set_group(move_group)
+
+
+groups_info = define_groups()
 groups = [Group(i)
-          for i in "123456789"]
-
-# Move in workspaces
-keys.extend([
-    Key([super_key],
-        "Right",
-        lazy.screen.next_group(),
-        desc="Move to next workspace"),
-    Key([super_key],
-        "Left",
-        lazy.screen.prev_group(),
-        desc="Move to previous workspace"),
-])
+          for i in groups_info]
+keys.extend(
+    [
+        # Move in workspaces
+        Key([super_key],
+            "Down",
+            lazy.screen.next_group(),
+            desc="Move to next workspace"),
+        Key([super_key],
+            "Up",
+            lazy.screen.prev_group(),
+            desc="Move to previous workspace"),
+        # Move windows to another workspace
+        Key([super_key, "shift"],
+            "Down",
+            displace_windows(groups_info,
+                             "next"),
+            desc="Move window to next workspace"),
+        Key([super_key, "shift"],
+            "Up",
+            displace_windows(groups_info,
+                             "prev"),
+            desc="Move window to previous workspace"),
+    ]
+)
 
 for i in groups:
     keys.extend(
@@ -27,18 +94,5 @@ for i in groups:
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [super_key, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
         ]
     )
-
-
