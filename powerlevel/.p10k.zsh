1af70eb108a938e01bfd3c50c933852d81305ae0
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-07-01 02:34:12 -0500

    Chnage colors

--- powerlevel/.p10k.zsh
+++ powerlevel/.p10k.zsh
@@ -182,8 +182,8 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=254
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=15
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=135
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
