e862fd02cc2ad0ebf3a3fafb1a5a5128ec044905
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-11-22 00:07:48 -0600

    Remove useless snippet, and move in wrap lines

--- nvim/.config/nvim/lua/configs/init.lua
+++ nvim/.config/nvim/lua/configs/init.lua
@@ -1,2 +1,7 @@
 require("configs.telescope")
 require("configs.lspconfig")
+require('spellsitter').setup {
+  -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
+  enable = true,
+  debug = false
+}
