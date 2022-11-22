28fdfc719107f2cea0c80faafdbb52e352c70a56
Giovanni Lopez <giovannilopez9808@gmail.com>
2022-11-22 00:09:12 -0600

    Remove useless plugin

--- nvim/.config/nvim/lua/configs/init.lua
+++ nvim/.config/nvim/lua/configs/init.lua
@@ -1,7 +1,2 @@
 require("configs.telescope")
 require("configs.lspconfig")
-require('spellsitter').setup {
-  -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
-  enable = true,
-  debug = false
-}
